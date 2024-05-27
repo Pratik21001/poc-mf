24928b8fa63729a5f55c1e3fbb32c5cef5fb4016
Pratik Katlana <pratik.katlana@blazeclan.com>
2024-05-27 17:24:50 +0530

    updated code

--- s3.tf
+++ s3.tf
@@ -3,6 +3,11 @@ resource "aws_s3_bucket" "lambda_s3_buckets" {
     force_destroy = true
 }
 
+resource "aws_s3_bucket" "pipeline_s3_buckets" {
+    bucket = "poc-mf-pratik-burn"
+    force_destroy = true
+}
+
 # resource "aws_s3_bucket_object" "object" {
 #     bucket = "poc-mf-lambda"
 #     key    = "lambda"
