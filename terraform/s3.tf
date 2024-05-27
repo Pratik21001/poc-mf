d8de7ee6a6381e95f6cfbbb44e51037c74e20b40
Pratik Katlana <pratik.katlana@blazeclan.com>
2024-05-27 15:59:59 +0530

    update

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,15 +2,14 @@ resource "aws_s3_bucket" "lambda_s3_buckets" {
     bucket = "poc-mf-lambda"
     acl    = "private"
     force_destroy = true
-
 }
 
-resource "aws_s3_bucket_object" "object" {
-    bucket = "poc-mf-lambda"
-    key    = "lambda"
-    source = "lambda_function_payload.zip"
+# resource "aws_s3_bucket_object" "object" {
+#     bucket = "poc-mf-lambda"
+#     key    = "lambda"
+#     source = "lambda_function_payload.zip"
 
-    depends_on = [
-    aws_s3_bucket.lambda_s3_buckets,
-    ]
-}
+#     depends_on = [
+#     aws_s3_bucket.lambda_s3_buckets,
+#     ]
+# }
