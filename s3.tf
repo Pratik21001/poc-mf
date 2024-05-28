898b734f425789aa8f0f5fa3c45a1e069c0a2a6b
Pratik Katlana <pratik.katlana@blazeclan.com>
2024-05-28 13:54:09 +0530

    sns testing

--- s3.tf
+++ s3.tf
@@ -1,24 +1,16 @@
-resource "aws_s3_bucket" "lambda_s3_buckets" {
-    bucket = "poc-mf-lambda"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "lambda_s3_buckets" {
+#     bucket = "poc-mf-lambda"
+#     force_destroy = true
+# }
 
-resource "aws_s3_bucket" "pipeline_s3_buckets" {
-    bucket = "poc-mf-pratik-burn"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_buckets" {
+#     bucket = "poc-mf-pratik-burn"
+#     force_destroy = true
+# }
 
 resource "aws_s3_bucket" "pipeline_s3_bucket_28" {
     bucket = "poc-mf-pratik-burn-28-05-2024"
     force_destroy = true
 }
 
-# resource "aws_s3_bucket_object" "object" {
-#     bucket = "poc-mf-lambda"
-#     key    = "lambda"
-#     source = "lambda_function_payload.zip"
 
-#     depends_on = [
-#     aws_s3_bucket.lambda_s3_buckets,
-#     ]
-# }
