6fe68aa2c44da385bc96f06223f43009dfd88f29
Pratik Katlana <pratik.katlana@blazeclan.com>
2024-06-10 12:00:54 +0530

    updated jenkins

--- s3.tf
+++ s3.tf
@@ -1,40 +1,31 @@
-# resource "aws_s3_bucket" "lambda_s3_buckets" {
-#     bucket = "poc-mf-lambda"
+
+resource "aws_s3_bucket" "pipeline_s3_bucket_10" {
+    bucket = "poc-mf-pratik-burn-10-06-2024"
+    force_destroy = true
+}
+
+
+# resource "aws_s3_bucket" "pipeline_s3" {
+#     bucket = "poc-mf-pratik-burn-31-07-2025"
 #     force_destroy = true
 # }
 
-# resource "aws_s3_bucket" "pipeline_s3_buckets" {
-#     bucket = "poc-mf-pratik-burn"
+# resource "aws_s3_bucket" "pipeline_s3_bucket-1" {
+#     bucket = "poc-mf-pratik-testing-bucket-1"
 #     force_destroy = true
 # }
 
-resource "aws_s3_bucket" "pipeline_s3_bucket_28" {
-    bucket = "poc-mf-pratik-burn-28-05-2024"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_bucket-2" {
+#     bucket = "poc-mf-pratik-testing-bucket-2"
+#     force_destroy = true
+# }
 
+# resource "aws_s3_bucket" "pipeline_s3_bucket-3" {
+#     bucket = "poc-mf-pratik-testing-bucket-3"
+#     force_destroy = true
+# }
 
-resource "aws_s3_bucket" "pipeline_s3" {
-    bucket = "poc-mf-pratik-burn-31-07-2025"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket" "pipeline_s3_bucket-1" {
-    bucket = "poc-mf-pratik-testing-bucket-1"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket" "pipeline_s3_bucket-2" {
-    bucket = "poc-mf-pratik-testing-bucket-2"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket" "pipeline_s3_bucket-3" {
-    bucket = "poc-mf-pratik-testing-bucket-3"
-    force_destroy = true
-}
-
-resource "aws_s3_bucket" "pipeline_s3_bucket-4" {
-    bucket = "poc-mf-pratik-testing-bucket-4"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_bucket-4" {
+#     bucket = "poc-mf-pratik-testing-bucket-4"
+#     force_destroy = true
+# }
