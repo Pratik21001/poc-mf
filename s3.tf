696e7af2a02229c43d2151bd3cf1b8c070ef667a
Pratik Katlana <pratik.katlana@blazeclan.com>
2024-06-12 16:18:02 +0530

    bucket updated

--- s3.tf
+++ s3.tf
@@ -10,20 +10,20 @@ resource "aws_s3_bucket" "pipeline_s3" {
     force_destroy = true
 }
 
-resource "aws_s3_bucket" "pipeline_s3_buckets" {
-    bucket = "poc-mf-pratik-burn-12-06-2027"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_buckets" {
+#     bucket = "poc-mf-pratik-burn-12-06-2027"
+#     force_destroy = true
+# }
 
-resource "aws_s3_bucket" "pipeline_s3_mahindra" {
-    bucket = "poc-mf-pratik-burn-19-09-2029"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_mahindra" {
+#     bucket = "poc-mf-pratik-burn-19-09-2029"
+#     force_destroy = true
+# }
 
-resource "aws_s3_bucket" "pipeline_s3_finance" {
-    bucket = "poc-mahindra-finance-24-07-2025"
-    force_destroy = true
-}
+# resource "aws_s3_bucket" "pipeline_s3_finance" {
+#     bucket = "poc-mahindra-finance-24-07-2025"
+#     force_destroy = true
+# }
 # resource "aws_s3_bucket" "pipeline_s3_bucket-1" {
 #     bucket = "poc-mf-pratik-testing-bucket-1"
 #     force_destroy = true
